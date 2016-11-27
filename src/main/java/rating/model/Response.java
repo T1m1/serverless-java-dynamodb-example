88cac651b01313ce72dfffc1636ced59f1da57f1
Timi <ruprecht.t@gmx.de>
2016-11-27 21:07:46 +0100

    use lombok

--- src/main/java/rating/model/Response.java
+++ src/main/java/rating/model/Response.java
@@ -1,32 +1,18 @@
 package rating.model;
 
+import lombok.Data;
+
+@Data
 public class Response {
 
-	private String message;
-	private Request request;
+    private String message;
+    private Request request;
 
-	public Response(String message, Request request) {
-		this.message = message;
-		this.request = request;
-	}
+    public Response(String message, Request request) {
+        this.message = message;
+        this.request = request;
+    }
 
-	public Response() {
-	}
-
-	public String getMessage() {
-		return this.message;
-	}
-
-	public Request getRequest() {
-		return this.request;
-	}
-
-	public void setMessage(String message) {
-		this.message = message;
-	}
-
-	public void setRequest(Request request) {
-		this.request = request;
-	}
-
+    public Response() {
+    }
 }
