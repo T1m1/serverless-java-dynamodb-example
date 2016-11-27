1795f021d0e16cfbee45dd208ced4b17b2b6e843
Timi <ruprecht.t@gmx.de>
2016-11-27 23:35:07 +0100

    implement handler to create rating entry in dynamoDB

--- src/main/java/rating/handler/CreateHandler.java
+++ src/main/java/rating/handler/CreateHandler.java
@@ -1,15 +1,37 @@
 package rating.handler;
 
+import com.amazonaws.regions.Region;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClient;
+import com.amazonaws.services.dynamodbv2.document.DynamoDB;
+import com.amazonaws.services.dynamodbv2.document.Table;
+import com.amazonaws.services.dynamodbv2.document.spec.PutItemSpec;
+import com.amazonaws.services.dynamodbv2.model.ConditionalCheckFailedException;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import rating.model.Rating;
 import rating.model.Response;
+import rating.util.DynamoDBHelper;
+import rating.util.Util;
 
 public class CreateHandler implements RequestHandler<Rating, Response> {
 
-	@Override
-	public Response handleRequest(Rating input, Context context) {
-		return new Response("Go Serverless v1.0! Your function executed successfully!", input);
-	}
+    public Response handleRequest(Rating rating, Context context) {
+        AmazonDynamoDBClient dynamoDBClient = new AmazonDynamoDBClient().withRegion(Region.getRegion(Regions.US_EAST_1));
+        DynamoDB dynamoDB = new DynamoDB(dynamoDBClient);
+        Table table = dynamoDB.getTable("serverless3");
+
+        Util.checkParameter(rating);
+
+        try {
+            PutItemSpec putItemSpec = new PutItemSpec().withItem(DynamoDBHelper.ratingToDBItem((Rating) rating));
+            table.putItem(putItemSpec);
+        } catch (ConditionalCheckFailedException e) {
+            throw new IllegalArgumentException("400 Bad Rating");
+
+        }
+        return new Response("Rating for charge station created!", rating);
+    }
+
 
 }
